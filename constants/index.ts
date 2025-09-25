b382d996407b26f291715d70786be30dbcbf9f57
Vladyslav Bondarchuk <vladjumper14@gmail.com>
2025-09-25 13:37:40 +0300

    change resume link

--- constants/index.ts
+++ constants/index.ts
@@ -2,23 +2,11 @@ import { AppRoutesEnum, AppRoutesLabelsEnum } from '@/types/enums';
 import { ILink } from '@/types/interfaces';
 
 export const links: ILink[] = [
-  {
-    path: AppRoutesEnum.EXPERIENCE,
-    label: AppRoutesLabelsEnum.EXPERIENCE,
-  },
-  {
-    path: AppRoutesEnum.PROJECTS,
-    label: AppRoutesLabelsEnum.PROJECTS,
-  },
-  {
-    path: AppRoutesEnum.SKILLS,
-    label: AppRoutesLabelsEnum.SKILLS,
-  },
-  {
-    path: AppRoutesEnum.CONTACT,
-    label: AppRoutesLabelsEnum.CONTACT,
-  },
+  { path: AppRoutesEnum.EXPERIENCE, label: AppRoutesLabelsEnum.EXPERIENCE },
+  { path: AppRoutesEnum.PROJECTS, label: AppRoutesLabelsEnum.PROJECTS },
+  { path: AppRoutesEnum.SKILLS, label: AppRoutesLabelsEnum.SKILLS },
+  { path: AppRoutesEnum.CONTACT, label: AppRoutesLabelsEnum.CONTACT },
 ];
 
 export const resumeLink =
-  'https://drive.google.com/file/d/1ylI_iGUa0ANvIH8k8c0NJ_LcEsnbZ8pB/view?usp=sharing';
+  'https://drive.google.com/file/d/1acmQ7TlIOVtpZBNmsc9QvvWFsvwyGHYC/view?usp=sharing';
